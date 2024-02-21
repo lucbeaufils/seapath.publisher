48f114717c8f06cff28e7f4f681ed7f21ff51910
Luc BEAUFILS <luc.beaufils@savoirfairelinux.com>
2024-02-21 14:20:03 +0100

    playbooks: files: add ci_publish_sv.sh script
    This script creates a process in one RT core to run `bittwist` for
    sending pcap and on the other side save network packages
    using `tcpdump` on another RT core.

---
+++ playbooks/files/ci_publish_sv.sh
@@ -0,0 +1,26 @@
+# Copyright (C) 2024 Savoir-faire Linux, Inc
+# SPDX-License-Identifier: Apache-2.0
+
+#!/bin/bash
+set -x
+
+# First parameter : pcap file
+# Second parameter : root directory
+# Third parameter : network interface
+
+CMD_RUNNER="bittwist -i $3 $2/pcaps/$1"
+
+CMD_DUMP="tcpdump -w $2/results/result-$(date +%s).pcap"
+
+$CMD_DUMP &
+PID_DUMP=$!
+
+$CMD_RUNNER &
+PID_RUNNER=$!
+
+wait $PID_RUNNER
+kill -2 $PID_DUMP
+
+wait $PID_DUMP
+
+exit 0
